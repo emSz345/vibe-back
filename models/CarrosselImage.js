d34ecdc13c2712cc8caa77a686047984431a38a6
Everton Souza <118739243+emSz345@users.noreply.github.com>
2025-09-24 08:12:14 -0300

    Update CarrosselImage.js

--- models/CarrosselImage.js
+++ models/CarrosselImage.js
@@ -1,18 +1,17 @@
+// models/CarrosselImage.js
 const mongoose = require('mongoose');
 
-// Define o schema do documento no MongoDB.
-// Neste caso, cada documento terá apenas o nome do arquivo da imagem.
 const carrosselImageSchema = new mongoose.Schema({
     filename: {
         type: String,
         required: true,
-        unique: true // Garante que não teremos nomes de arquivos duplicados
+        unique: true
     },
-    // Você pode adicionar outros campos aqui se precisar, como a data do upload
-    // uploadDate: {
-    //     type: Date,
-    //     default: Date.now
-    // }
+    eventoId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Event',
+        required: false
+    }
 });
 
 const CarrosselImage = mongoose.model('CarrosselImage', carrosselImageSchema);
